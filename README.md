d5ecb0e17e356cc7ee8a1a5c9c675840c29f70bb
gabrielgarib1 <gabrielgaribg@gmail.com>
2025-11-14 15:43:00 -0300

    simple UDP that transmit one message max 1MB to a local port

---
+++ README.md
@@ -0,0 +1,3 @@
+## ANOTATIONS
+
+test if port isnt used with netstat -an | grep :"""""my_port""""
---
+++ servidor0_UDP.py
@@ -0,0 +1,18 @@
+
+
+import socket
+
+
+my_ip=''    #any interface
+my_port=5000    #port to listen on
+
+udp=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp.bind((my_ip,my_port))
+
+max_message_size=1   #in megabytes
+received_data, addr=udp.recvfrom(2**(8*max_message_size))  
+
+
+print("Message: ", received_data.decode(), "| received from:", addr)
+
+udp.close()
